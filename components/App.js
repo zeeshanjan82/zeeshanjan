dddac929284196127c1f05d0609df9cd358ba4e6
Zeeshan Jan <zeeshan@zeeshanjan.com>
2023-12-12 17:37:37 +0530

    Making changes

--- components/App.js
+++ components/App.js
@@ -4,7 +4,7 @@ const App = () => {
         <div className="text-3xl underline">
             I will be back soon!      
         </div>
-        <div>A little patience...</div>
+        <div>A little patience..</div>
         <div>For now, my website is under construction as I am adding some very cool features :) I am doing my best to finish this upgrade asap.</div>
     </div>;
 };
